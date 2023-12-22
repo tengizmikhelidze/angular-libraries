ff5ec2ba299e904fe0924b4cd9f56611fa9d153d
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-12-22 17:34:27 +0400

    fix - colors

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,7 @@ import {faFacebook, faInstagram} from "@fortawesome/free-brands-svg-icons";
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  readonly facebookIcon = faFacebook
-  readonly instagramIcon = faInstagram
+  readonly environment = environment;
+  readonly facebookIcon = faFacebook;
+  readonly instagramIcon = faInstagram;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  facebookUrl: 'https://www.facebook.com/profile.php?id=100095031687801'
+  facebookUrl: 'https://www.facebook.com/profile.php?id=100095031687801',
+  instagram: 'https://www.instagram.com/angular.georgia/'
 };
